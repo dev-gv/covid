a628191e5959346a92bd2f9f2f39fcd11801222f
Baddy <dev.gauravverma@gmail.com>
2020-12-03 13:50:01 +0530

    Added a final Description on File

--- .ipynb_checkpoints/Covid-checkpoint.py
+++ .ipynb_checkpoints/Covid-checkpoint.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 12:31:40 2020
+
+@author: Gaurav Verma
+
+This Module Contains Covid Cases Data taken From https://www.worldometers.info/coronavirus/
+
+Functions and their breif Detail :
+
+1- Track_str() : This Function returns Raw Data of All Countries in String Data Type
+
+-------------------------------------------------------------------------------
+2- Track() : This Function returns Raw Data of All Countries in integer Data Type
+
+-------------------------------------------------------------------------------
+3- Main() and Co_main() : these functions are The Backbone of All Functions
+   contains data used to Create a Table
+
+-------------------------------------------------------------------------------
+4- active() : this function gives the Total Active Cases in That Day
+
+-------------------------------------------------------------------------------
+5- death_recovery_rate() : this Function have two Columns Death Rate and Recovery Rate on That day
+
+-------------------------------------------------------------------------------
+6- tatal_deaths() : this Function gives total death upto that day and Tatal Death on That day
+
+# =============================================================================
+# """
 import pandas as pd
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
@@ -54,10 +85,10 @@ def Track_str():
             data=data.append(temp,ignore_index=True)
     data.drop(["SKIP"],inplace=True,axis=1)
     return data
-<<<<<<< HEAD
-=======
+
+
 #create a Data frame
->>>>>>> Text
+
 def Track():
     '''
     Print the current Covid19 cases in Integer Dtype 
@@ -102,7 +133,6 @@ def Track():
             data=data.append(temp,ignore_index=True)
     data.drop(["SKIP"],inplace=True,axis=1)
     return data
-<<<<<<< HEAD
 
 def main_function(x):
     '''
@@ -122,6 +152,56 @@ def main_function(x):
     final_active_world = Z[i+1:j-1]
     final_active_world = list(map(float,final_active_world.replace("\"","").split(',')))
     
+
     return final_date,final_active_world
-=======
->>>>>>> Text
+
+def co_main(x,lable=None):
+    t=soup.find_all(type="text/javascript")[x].string
+    i=re.search(lable,t).span()[1]
+    Z=t[i:]
+    (i,j)=re.search(r"\[(.*?)\]",Z).span()
+    final_date = Z[i+1:j-1]
+    final_date = final_date.replace("null","0")
+    final_active_world = list(map(float,final_date.replace("\"","").split(',')))
+
+    return final_active_world
+def active():
+    '''
+    This Method Returns the Day Wise Current Active Cases in World
+    
+    PS : Remember this is Total Day wise Cases
+    '''
+    a,b = main_function(7)
+    return pd.DataFrame({"Date":a , "Active_cases":b})
+
+def death_recovery_rate():
+    '''
+    This Method Returns the Day Wise Current Death Rate and Recovery Rate in World
+    
+    PS : Remember this is Total Death Rate
+    '''
+    a,b = main_function(8)
+    recovery_rate = co_main(8,"Recovery Rate")
+    return pd.DataFrame({"Date":a , "Death_rate":b,"Recovey_rate":recovery_rate})
+
+def total_cases():
+    '''
+    This Method Returns the Day Wise Current Active Cases in World
+    
+    PS : Remember this is Total Day wise Cases
+    '''
+    a,b = main_function(9)
+    daily_cases = co_main(9,"Daily Cases")
+    return pd.DataFrame({"Date":a , "Active_cases":b,"Daily_cases":daily_cases})
+def total_deaths():
+    '''
+    This Method Returns the Day Wise Current Deaths and Daily Deaths in World
+    
+    return : DataFrame With date,Total Deaths,Daily Deaths
+    
+    '''
+    a,b = main_function(10)
+    c=[0]
+    for i in range(1,len(b)):
+        c.append(b[i]-b[i-1])
+    return pd.DataFrame({"Date":a,"Deaths":b,"Daily Deaths":c})
